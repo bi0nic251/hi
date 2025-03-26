4b156638ca926ff1c3c95d00b44474b6437dd09c
bi0nic251 <nolan.phillips@meritknights.com>
2025-03-26 14:03:50 -0600

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,15 +1,15 @@
 package com.example.dirtswordmod;
 
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemSword;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
+import net.lax1dude.eaglercraft.v1_8.minecraft.creativetab.CreativeTabs;
+import net.lax1dude.eaglercraft.v1_8.minecraft.item.Item;
+import net.lax1dude.eaglercraft.v1_8.minecraft.item.ItemSword;
+import net.lax1dude.eaglercraft.v1_8.minecraftforge.fml.common.Mod;
+import net.lax1dude.eaglercraft.v1_8.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.lax1dude.eaglercraft.v1_8.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.lax1dude.eaglercraft.v1_8.minecraftforge.fml.common.registry.GameRegistry;
+import net.lax1dude.eaglercraft.v1_8.minecraft.init.Blocks;
+import net.lax1dude.eaglercraft.v1_8.minecraft.init.Items;
+import net.lax1dude.eaglercraft.v1_8.minecraft.item.ItemStack;
 
 @Mod(modid = DirtSwordMod.MODID, version = DirtSwordMod.VERSION, name = DirtSwordMod.NAME)
 public class DirtSwordMod {
@@ -37,6 +37,12 @@ public class DirtSwordMod {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        GameRegistry.addRecipe(new ItemStack(dirtSword), " D ", " D ", " S ", 'D', Blocks.DIRT, 'S', Items.STICK);
+        GameRegistry.addShapedRecipe(new ItemStack(dirtSword), 
+            " D ", 
+            " D ", 
+            " S ", 
+            'D', Blocks.DIRT, 
+            'S', Items.STICK
+        );
     }
 }
